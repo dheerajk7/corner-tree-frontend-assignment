61d326365e378a98397ca74157db9ceabee2ca4b
Dheeraj Kushwah <dheeraj77253@gmail.com>
2020-10-18 18:17:14 +0530

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../styles/App.scss';
+
+class App extends Component {
+  render() {
+    return <div className="App">Working</div>;
+  }
+}
+
+export default App;
